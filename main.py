42124904eccccb78923f3c1dad51f764f0099cc8
Peter Idoko <Peter_Idoko@vacfss.com>
2025-01-30 13:20:26 -0800

    added bounding box around the face in the image

--- main.py
+++ main.py
@@ -1,5 +1,29 @@
 import cv2
+import matplotlib.pyplot as plt
 
 imagePath = 'input_image.jpg'
 
 img = cv2.imread(imagePath)
+
+print(f"Shape properties of img: {img.shape}\n")
+
+gray_image = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+print(f"Shape properties of gray_image: {gray_image.shape}\n")
+
+face_classifier = cv2.CascadeClassifier(
+    cv2.data.haarcascades + "haarcascade_frontalfae_default.xml"
+)
+
+face = face_classifier.detectMultiScale(
+    gray_image, scaleFactor=1.1, minNeighbors=5, minSize=(40, 40)
+)
+
+for (x, y, w, h) in face:
+    cv2.rectangle(img, (x,y), (x + w, y + h), (0, 255, 0), 4)
+
+img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+
+plt.figure(figsize=(20,10))
+plt.imshow(img_rgb)
+plt.axis('off')
